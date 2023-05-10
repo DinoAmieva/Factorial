c85cd99527551339977df85c5282fa9b0736e239
Dino Amieva <125599893+DinoAmieva@users.noreply.github.com>
2023-05-09 18:54:11 -0600

    Add files via upload

---
+++ Factorial/src/factorial/Factorial.java
@@ -0,0 +1,35 @@
+
+package factorial;
+import java.util.Scanner;
+
+/**
+ *
+ * 
+@author
+ maryse
+ *
+ * Ciclo for
+ * Programa para el cálculo del fatorial de un número
+ *
+ */
+
+public class Factorial {
+        Scanner stdIn = new Scanner(System.in);
+        int numero;
+        double factorial = 1.0;
+                
+    public void CalculoFactorial(String[] args) {
+
+        System.out.print("Introduzca un número entero: ");
+        numero = stdIn.nextInt();
+        for (int i = 2; i <= numero; i++) {
+            factorial *= i;
+        }
+        System.out.println(numero + "! = " + factorial);
+    }
+    
+    public static void main(String[] args) {
+        Factorial llamar = new Factorial();
+        llamar.CalculoFactorial(args);
+    }
+}
